c721bf03fa9da669980b4686fec68a275a4c0119
minearlab <50639052+minearlab@users.noreply.github.com>
2019-05-15 12:59:32 -0600

    Add files via upload

---
+++ VRInput/VRInput.cs
@@ -0,0 +1,529 @@
+﻿using System;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+
+/*************************************************************************************************/
+/* 
+
+WARNING:  
+  This particular utility requires that the "Vive Input Plugin" be installed.
+  You can find it on the Unity Store for free
+
+
+WARNING:
+  This utility is set up for the HTC Vive but, the "Vive Input Plugin" also works with Occulus.
+  If you need Occulus support, you will need to add it
+
+
+INFORMATION:
+
+      WRITTEN BY:     Adrian T. Barberis -- 14abarberis@gmail.com
+      CREATED:        2/4/2019
+      LAST EDIT:      2/4/2019
+      FOR:            Dr. Meredith Minear, University of Wyoming
+*/
+/*************************************************************************************************/
+
+
+namespace Inputs
+{
+    public enum ButtonStatus { Pressed, Released }
+
+
+    [RequireComponent(typeof(Valve.VR.InteractionSystem.Hand))]
+    public class VRInput : MonoBehaviour
+    {
+        public event EventHandler<VRInputEventArgs> GripPressed;
+        public event EventHandler<VRInputEventArgs> TriggerTapped;
+        public event EventHandler<VRInputEventArgs> TriggerHeld;
+        public event EventHandler<VRInputEventArgs> TriggerClicked;
+        public event EventHandler<VRInputEventArgs> PadPressed;
+        public event EventHandler<VRInputEventArgs> PadTouched;
+        public event EventHandler<VRInputEventArgs> MenuPressed;
+        public event EventHandler<VRInputEventArgs> GripReleased;
+        public event EventHandler<VRInputEventArgs> MenuReleased;
+        public event EventHandler<VRInputEventArgs> PadReleased;
+        public event EventHandler<VRInputEventArgs> TriggerReleased;
+
+        private void Update()
+        {
+            // Grip Press Checks
+            CheckGripPressed(HandRole.RightHand);
+            CheckGripPressed(HandRole.LeftHand);
+            //------------------------------------
+
+
+            // Menu Button Press Checks
+            CheckMenuPressed(HandRole.RightHand);
+            CheckMenuPressed(HandRole.LeftHand);
+            //------------------------------------
+
+
+            // Pad Press Checks
+            CheckPadPressed(HandRole.RightHand);
+            CheckPadPressed(HandRole.LeftHand);
+
+            CheckPadTouched(HandRole.RightHand);
+            CheckPadTouched(HandRole.LeftHand);
+            //------------------------------------
+
+
+            // Trigger Press Checks
+            CheckTriggerHeld(HandRole.RightHand);
+            CheckTriggerHeld(HandRole.LeftHand);
+
+            CheckTriggerTapped(HandRole.RightHand);
+            CheckTriggerTapped(HandRole.LeftHand);
+
+            CheckTriggerClicked(HandRole.RightHand);
+            CheckTriggerClicked(HandRole.LeftHand);
+            //------------------------------------
+
+
+            // Grip Release Checks
+            CheckGripReleased(HandRole.RightHand);
+            CheckGripReleased(HandRole.LeftHand);
+            //------------------------------------
+
+
+            // Menu Button Release Checks
+            CheckMenuReleased(HandRole.RightHand);
+            CheckMenuReleased(HandRole.LeftHand);
+            //------------------------------------
+
+
+            // Pad Release Checks
+            CheckPadReleased(HandRole.RightHand);
+            CheckPadReleased(HandRole.LeftHand);
+            //------------------------------------
+
+
+            // Trigger Release Checks
+            CheckTriggerReleased(HandRole.RightHand);
+            CheckTriggerReleased(HandRole.LeftHand);
+            //------------------------------------
+
+        }
+
+
+
+        #region /* Check For Button Presses */
+        //+------------------------+
+        //| Check For Grip Pressed |
+        //+------------------------+
+        private void CheckGripPressed(HandRole hr)
+        {
+            if(ViveInput.GetPress(hr, ControllerButton.Grip))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.Grip;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnGripPressed(args);
+            }
+        }
+        //================================================================
+
+
+
+        //+------------------------+
+        //| Check For Menu Pressed |
+        //+------------------------+
+        private void CheckMenuPressed(HandRole hr)
+        {
+            if (ViveInput.GetPressDown(hr, ControllerButton.Menu))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.Menu;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnMenuPressed(args);
+            }
+        }
+        //================================================================
+
+
+
+        //+-----------------------+
+        //| Check For Pad Pressed |
+        //+-----------------------+
+        private void CheckPadPressed(HandRole hr)
+        {
+            if (ViveInput.GetPressDown(hr, ControllerButton.Pad))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.Pad;
+                args.HandRole = hr;
+                args.PressCoordinates = ViveInput.GetPadAxis(hr);
+                args.Timestamp = DateTime.Now;
+                OnPadPressed(args);
+            }
+        }
+        //================================================================
+
+
+
+        //+-----------------------+
+        //| Check For Pad Touched |
+        //+-----------------------+
+        private void CheckPadTouched(HandRole hr)
+        {
+            if (ViveInput.GetPressDown(hr, ControllerButton.PadTouch))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.PadTouch;
+                args.HandRole = hr;
+                args.TouchCoordinates = ViveInput.GetPadAxis(hr);
+                args.Timestamp = DateTime.Now;
+                OnPadTouched(args);
+            }
+        }
+        //================================================================
+
+
+
+        //+----------------------------------+
+        //| Check For Trigger Pressed / Held |
+        //+----------------------------------+
+        private void CheckTriggerHeld(HandRole hr)
+        {
+            if (ViveInput.GetPressDown(hr, ControllerButton.Trigger))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.Trigger;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnTriggerHeld(args);
+            }
+        }
+        //================================================================
+
+
+
+        //+-----------------------------------------------------+
+        //| Check For Trigger Tapped / Light Press / Light Hold |
+        //+-----------------------------------------------------+
+        private void CheckTriggerTapped(HandRole hr)
+        {
+            if (ViveInput.GetPressDown(hr, ControllerButton.TriggerTouch))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.TriggerTouch;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnTriggerTapped(args);
+            }
+        }
+        //================================================================
+
+
+
+        //+----------------------------------------------------+
+        //| Check For Trigger Clicked / Full Press / Full Hold |
+        //+----------------------------------------------------+
+        private void CheckTriggerClicked(HandRole hr)
+        {
+            if (ViveInput.GetPressDown(hr, ControllerButton.FullTrigger))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.FullTrigger;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnTriggerClicked(args);
+            }
+        }
+        //================================================================
+        #endregion
+
+
+        #region /* Check For Button Releases */
+        //+------------------------+
+        //| Check For Grip Release |
+        //+------------------------+
+        private void CheckGripReleased(HandRole hr)
+        {
+            if (ViveInput.GetPressUp(hr, ControllerButton.Grip))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.Grip;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnGripReleased(args);
+            }
+        }
+        //================================================================
+
+
+
+        //+------------------------+
+        //| Check For Menu Release |
+        //+------------------------+
+        private void CheckMenuReleased(HandRole hr)
+        {
+            if (ViveInput.GetPressUp(hr, ControllerButton.Menu))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.Menu;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnMenuReleased(args);
+            }
+        }
+        //================================================================
+
+
+
+        //+-----------------------+
+        //| Check For Pad Release |
+        //+-----------------------+
+        private void CheckPadReleased(HandRole hr)
+        {
+            if (ViveInput.GetPressUp(hr, ControllerButton.Pad))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.Pad;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnPadReleased(args);
+            }
+            else if(ViveInput.GetPressUp(hr, ControllerButton.PadTouch))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.PadTouch;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnPadReleased(args);
+            }
+        }
+        //================================================================
+
+
+
+        //+---------------------------+
+        //| Check For Trigger Release |
+        //+---------------------------+
+        private void CheckTriggerReleased(HandRole hr)
+        {
+            if (ViveInput.GetPressUp(hr, ControllerButton.Trigger))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.Trigger;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnTriggerReleased(args);
+            }
+            else if(ViveInput.GetPressUp(hr, ControllerButton.TriggerTouch))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.TriggerTouch;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnTriggerReleased(args);
+            }
+            else if (ViveInput.GetPressUp(hr, ControllerButton.FullTrigger))
+            {
+                VRInputEventArgs args = new VRInputEventArgs();
+                args.ButtonPressed = ControllerButton.FullTrigger;
+                args.HandRole = hr;
+                args.Timestamp = DateTime.Now;
+                OnTriggerReleased(args);
+            }
+        }
+        //================================================================
+        #endregion
+
+
+        #region /* "OnPressed" Signals */
+
+        //+---------------------+
+        //| Signal Grip Pressed |
+        //+---------------------+
+        protected virtual void OnGripPressed(VRInputEventArgs e)
+        {
+            EventHandler<VRInputEventArgs> handler = GripPressed;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+
+
+
+        //+---------------------+
+        //| Signal Menu Pressed |
+        //+---------------------+
+        protected virtual void OnMenuPressed(VRInputEventArgs e)
+        {
+            EventHandler<VRInputEventArgs> handler = MenuPressed;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+
+
+
+        //+--------------------+
+        //| Signal Pad Pressed |
+        //+--------------------+
+        protected virtual void OnPadPressed(VRInputEventArgs e)
+        {
+            EventHandler<VRInputEventArgs> handler = PadPressed;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+
+
+
+        //+--------------------+
+        //| Signal Pad Touched |
+        //+--------------------+
+        protected virtual void OnPadTouched(VRInputEventArgs e)
+        {
+            EventHandler<VRInputEventArgs> handler = PadTouched;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+
+
+
+        //+-------------------------------+
+        //| Signal Trigger Held / Pressed |
+        //+-------------------------------+
+        protected virtual void OnTriggerHeld(VRInputEventArgs e)
+        {
+            /* Activates If:
+             *      1) Trigger if Held at least to 55% of the way
+             *      2) Trigger is Pressed at least to 55% of the way
+             */
+
+            EventHandler <VRInputEventArgs> handler = TriggerHeld;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+
+
+
+        //+--------------------------------------------------+
+        //| Signal Trigger Tapped / Light Press / Light Hold |
+        //+--------------------------------------------------+
+        protected virtual void OnTriggerTapped(VRInputEventArgs e)
+        {
+            /* Activates If:
+             *      1) Trigger if Held at least to 25% of the way
+             *      2) Trigger is Pressed at least to 25% of the way
+             */
+            EventHandler<VRInputEventArgs> handler = TriggerTapped;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+
+
+
+        //+-------------------------------------------------+
+        //| Signal Trigger Clicked / Full Press / Full Hold |
+        //+-------------------------------------------------+
+        protected virtual void OnTriggerClicked(VRInputEventArgs e)
+        {
+            /* Activates If:
+             *      1) Trigger if Held at least to 100% of the way
+             *      2) Trigger is Pressed at least to 100% of the way
+             */
+            EventHandler<VRInputEventArgs> handler = TriggerClicked;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+        #endregion
+
+
+        #region /* "On Release" Signals */
+        //+----------------------+
+        //| Signal Grip Released |
+        //+----------------------+
+        protected virtual void OnGripReleased(VRInputEventArgs e)
+        {
+            EventHandler<VRInputEventArgs> handler = GripReleased;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+
+
+
+        //+----------------------+
+        //| Signal Menu Released |
+        //+----------------------+
+        protected virtual void OnMenuReleased(VRInputEventArgs e)
+        {
+            EventHandler<VRInputEventArgs> handler = MenuReleased;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+
+
+
+        //+---------------------+
+        //| Signal Pad Released |
+        //+---------------------+
+        protected virtual void OnPadReleased(VRInputEventArgs e)
+        {
+            EventHandler<VRInputEventArgs> handler = PadReleased;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+
+
+
+        //+---------------------+
+        //| Signal Pad Released |
+        //+---------------------+
+        protected virtual void OnTriggerReleased(VRInputEventArgs e)
+        {
+            EventHandler<VRInputEventArgs> handler = TriggerReleased;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //================================================================
+        #endregion
+
+
+    }
+
+    public class VRInputEventArgs : EventArgs
+    {
+        public ControllerButton ButtonPressed { get; set; }
+        public HandRole HandRole { get; set; }
+        public ButtonStatus Status {get; set;}
+        public Vector2 TouchCoordinates { get; set; }
+        public Vector2 PressCoordinates { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
+
---
+++ VRInput/VRInputHandler.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*************************************************************************************************/
+/* 
+
+WARNING:  
+  This particular utility requires that the "Vive Input Plugin" be installed.
+  You can find it on the Unity Store for free
+
+
+WARNING:
+  This utility is set up for the HTC Vive but, the "Vive Input Plugin" also works with Occulus.
+  If you need Occulus support, you will need to add it
+
+
+INFORMATION:
+
+      WRITTEN BY:     Adrian T. Barberis -- 14abarberis@gmail.com
+      CREATED:        2/4/2019
+      LAST EDIT:      2/6/2019
+      FOR:            Dr. Meredith Minear, University of Wyoming
+*/
+/*************************************************************************************************/
+
+namespace Inputs
+{
+    public enum Interaction { GripPressed,
+                              GripReleased,
+                              MenuPressed,
+                              MenuReleased,
+                              PadPressed,
+                              PadTouched,
+                              PadReleased,
+                              TriggerHeld,
+                              TriggerTapped,
+                              TriggerClicked,
+                              TriggerReleased
+                             }
+
+
+    [RequireComponent(typeof(VRInput))]
+    public class VRInputHandler : MonoBehaviour
+    {
+        private VRInput m_VRInput;
+        private static Dictionary<Interaction, VRInputEventArgs> m_Inputs = new Dictionary<Interaction, VRInputEventArgs>()
+        {
+            {Interaction.GripPressed, null },
+            {Interaction.GripReleased, null },
+            {Interaction.MenuPressed, null },
+            {Interaction.MenuReleased, null },
+            {Interaction.PadPressed, null },
+            {Interaction.PadTouched, null },
+            {Interaction.PadReleased, null },
+            {Interaction.TriggerHeld, null },
+            {Interaction.TriggerTapped, null },
+            {Interaction.TriggerClicked, null },
+            {Interaction.TriggerReleased, null }
+        };
+
+
+        private void Awake(){ Subscribe(); }
+        private void OnEnable(){ Subscribe(); }
+        private void OnDisable(){ Unsubscribe(); }
+        private void OnDestroy(){ Unsubscribe(); }
+
+
+        private void Unsubscribe()
+        {
+            m_VRInput.GripPressed -= OnGripPressed;
+            m_VRInput.GripReleased -= OnGripReleased;
+
+
+            m_VRInput.MenuPressed -= OnMenuPressed;
+            m_VRInput.MenuReleased -= OnMenuReleased;
+
+
+            m_VRInput.PadPressed -= OnPadPressed;
+            m_VRInput.PadTouched -= OnPadTouched;
+            m_VRInput.PadReleased -= OnPadReleased;
+
+
+            m_VRInput.TriggerTapped -= OnTriggerTapped;
+            m_VRInput.TriggerHeld -= OnTriggerHeld;
+            m_VRInput.TriggerClicked -= OnTriggerClicked;
+            m_VRInput.TriggerReleased -= OnTriggerReleased;
+        }
+        private void Subscribe()
+        {
+            m_VRInput = GetComponent<VRInput>();
+            if (m_VRInput == null)
+            {
+                Debug.LogError("Controller is missing VRInput Component");
+                return;
+            }
+
+            m_VRInput.GripPressed += OnGripPressed;
+            m_VRInput.GripReleased += OnGripReleased;
+
+
+            m_VRInput.MenuPressed += OnMenuPressed;
+            m_VRInput.MenuReleased += OnMenuReleased;
+
+
+            m_VRInput.PadPressed += OnPadPressed;
+            m_VRInput.PadTouched += OnPadTouched;
+            m_VRInput.PadReleased += OnPadReleased;
+
+
+            m_VRInput.TriggerTapped += OnTriggerTapped;
+            m_VRInput.TriggerHeld += OnTriggerHeld;
+            m_VRInput.TriggerClicked += OnTriggerClicked;
+            m_VRInput.TriggerReleased += OnTriggerReleased;
+        }
+
+
+        //+-------------+
+        //| Grip Events |
+        //+-------------+
+        private void OnGripPressed(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Grip Pressed");
+            m_Inputs[Interaction.GripPressed] = e;
+            m_Inputs[Interaction.GripReleased] = null;
+        }
+        private void OnGripReleased(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Grip Released");
+            m_Inputs[Interaction.GripReleased] = e;
+            m_Inputs[Interaction.GripPressed] = null;
+        }
+        //================================================================
+
+
+
+        //+--------------------+
+        //| Menu Button Events |
+        //+--------------------+
+        private void OnMenuPressed(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Menu Button Pressed");
+            m_Inputs[Interaction.MenuPressed] = e;
+            m_Inputs[Interaction.MenuReleased] = null;
+        }
+        private void OnMenuReleased(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Menu Button Released");
+            m_Inputs[Interaction.MenuReleased] = e;
+            m_Inputs[Interaction.MenuPressed] = null;
+        }
+        //================================================================
+
+
+
+        //+------------+
+        //| Pad Events |
+        //+------------+
+        private void OnPadPressed(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Pad Pressed");
+            m_Inputs[Interaction.PadPressed] = e;
+            m_Inputs[Interaction.PadTouched] = null;
+            m_Inputs[Interaction.PadReleased] = null;
+        }
+        private void OnPadTouched(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Pad Touched");
+            m_Inputs[Interaction.PadTouched] = e;
+            m_Inputs[Interaction.PadPressed] = null;
+            m_Inputs[Interaction.PadReleased] = null;
+        }
+        private void OnPadReleased(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Pad Released");
+            m_Inputs[Interaction.PadReleased] = e;
+            m_Inputs[Interaction.PadPressed] = null;
+            m_Inputs[Interaction.PadTouched] = null;
+        }
+        //================================================================
+
+
+
+        //+----------------+
+        //| Trigger Events |
+        //+----------------+
+        private void OnTriggerHeld(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Trigger Pressed/Held");
+            m_Inputs[Interaction.TriggerHeld] = e;
+            m_Inputs[Interaction.TriggerClicked] = null;
+            m_Inputs[Interaction.TriggerTapped] = null;
+            m_Inputs[Interaction.TriggerReleased] = null;
+        }
+        private void OnTriggerTapped(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Trigger Tapped/Light Hold");
+            m_Inputs[Interaction.TriggerTapped] = e;
+            m_Inputs[Interaction.TriggerClicked] = null;
+            m_Inputs[Interaction.TriggerHeld] = null;
+            m_Inputs[Interaction.TriggerReleased] = null;
+        }
+        private void OnTriggerClicked(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Trigger Clicked/Full Press/Ful Hold");
+            m_Inputs[Interaction.TriggerClicked] = e;
+            m_Inputs[Interaction.TriggerHeld] = null;
+            m_Inputs[Interaction.TriggerTapped] = null;
+            m_Inputs[Interaction.TriggerReleased] = null;
+        }
+        private void OnTriggerReleased(object sender, VRInputEventArgs e)
+        {
+            Debug.Log(e.HandRole + " Trigger Released");
+            m_Inputs[Interaction.TriggerReleased] = e;
+            m_Inputs[Interaction.TriggerClicked] = null;
+            m_Inputs[Interaction.TriggerTapped] = null;
+            m_Inputs[Interaction.TriggerHeld] = null;
+        }
+        //================================================================
+
+
+        public static VRInputEventArgs GripPressed() { return m_Inputs[Interaction.GripPressed]; }
+        public static VRInputEventArgs GripReleased() { return m_Inputs[Interaction.GripReleased]; }
+        public static VRInputEventArgs MenuPressed() { return m_Inputs[Interaction.MenuPressed]; }
+        public static VRInputEventArgs MenuReleased() { return m_Inputs[Interaction.MenuReleased]; }
+        public static VRInputEventArgs PadPressed() { return m_Inputs[Interaction.PadPressed]; }
+        public static VRInputEventArgs PadTouched() { return m_Inputs[Interaction.PadTouched]; }
+        public static VRInputEventArgs PadReleased() { return m_Inputs[Interaction.PadReleased]; }
+        public static VRInputEventArgs TriggerHeld() { return m_Inputs[Interaction.TriggerHeld]; }
+        public static VRInputEventArgs TriggerTapped() { return m_Inputs[Interaction.TriggerTapped]; }
+        public static VRInputEventArgs TriggerClicked() { return m_Inputs[Interaction.TriggerClicked]; }
+        public static VRInputEventArgs TriggerReleased() { return m_Inputs[Interaction.TriggerReleased]; }
+
+    }
+}
+
